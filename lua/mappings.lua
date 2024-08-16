80bd1a21c4a908b4192bd8c1d110a25390af928b
hchchiu <robert890619@gmail.com>
2024-08-17 00:12:43 +0800

    Update for resizing window key mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- NVChad Terminal
 map({"i", "n", "t"}, "<leader>j", function ()
   require("nvchad.term").toggle { pos = "float",
                                   id = "floatTerm",
@@ -19,5 +20,9 @@ map({"i", "n", "t"}, "<leader>j", function ()
                                     border = "single"
                                   }
   } end, { desc = "terminal toggle floating term" })
+
+--  Resize window
+map ({"i", "n"}, "]]", "<C-w>5>", {desc = "Resize window to left"})
+map ({"i", "n"}, "[[", "<C-w>5<", {desc = "Resize window to right"})
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
