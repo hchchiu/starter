debdb555e6c9792bb07a6f7c03a13b6c0a6dcf9e
hchchiu <robert890619@gmail.com>
2024-09-10 20:58:38 +0800

    set the catppuccin theme

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,16 +5,17 @@
 local M = {}
 
 M.base46 = {
+    theme = "catppuccin",
     hl_override = {
         Comment = { italic = true },
         ["@comment"] = { italic = true },
     },
 }
 
-M.ui {
-    tabufline = {
-        enabled = false,
-    },
-}
+-- M.ui {
+--     tabufline = {
+--         enabled = false,
+--     },
+-- }
 
 return M
